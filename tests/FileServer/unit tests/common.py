c5d19bb31743fc0d78f34266b012bf369c32c834
Connor Turlan <connorturlan@gmail.com>
2022-09-22 22:43:10 +0930

    fixed test path errors, fixed branch test

--- tests/FileServer/unit tests/common.py
+++ tests/FileServer/unit tests/common.py
@@ -2,6 +2,11 @@ import unittest, os, shutil
 from src.file_server import FileServer
 
 
+def mkdir(path):
+	if not os.path.exists(path):
+		os.mkdir(path)
+
+
 class DummyRouter(object):
 
 	def noop(*args, **kwargs):
@@ -15,16 +20,19 @@ class FileServerCommonTest(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(self):
-		self.root = './.safe harbour/'
+		self.root = os.path.join('./', '.safe harbour/')
 
 		# create a testing dir, wrapped in a safe folder so we don't accidentally delete tests.
-		os.mkdir(self.root)
-		os.mkdir(self.root + '.testing')
-		os.mkdir(self.root + '.canary')
+		mkdir(self.root)
+		mkdir(self.root + '.canary')
+		mkdir(self.root + '.testing')
+		mkdir(self.root + '.testing/folder')
 
 		# create a test file.
-		os.write(self.root + '.testing/test.txt',
-		         bytes('hello, world!\n', 'utf-8'))
+		with open(self.root + '.testing/test.txt', 'w') as file:
+			file.write('hello, world!\n')
+		with open(self.root + '.testing/folder/file.txt', 'w') as file:
+			file.write('The quick brown fox jumps over the lazy dog.')
 
 		self.server = FileServer('/files', self.root + '.testing')
 		self.dummy_router = DummyRouter()
@@ -32,8 +40,8 @@ class FileServerCommonTest(unittest.TestCase):
 	@classmethod
 	def tearDownClass(self):
 		# remove the testing directories.
-		shutil.rmtree('./.safe harbour')
-		shutil.rmtree('./.canary')
+		""" shutil.rmtree('./.safe harbour')
+		shutil.rmtree('./.canary') """
 
 
 if __name__ == "__main__":
--- tests/FileServer/unit tests/method_test.py
+++ tests/FileServer/unit tests/method_test.py
@@ -7,11 +7,34 @@ from .common import DummyRouter, FileServerCommonTest
 class FileServerGeneralUnitTests(FileServerCommonTest):
 	# test get file tree
 	def test_getFileTree_correct(self):
-		pass
+		test_tree = self.server.get_files_tree(self.root + '.testing/')
+		valid_tree = {
+		    'folder': {
+		        '.': ['file.txt'],
+		        '..': ['folder']
+		    },
+		    '.': ['test.txt'],
+		    '..': ['']
+		}
+
+		self.assertIsNotNone(test_tree)
+		self.assertDictEqual(valid_tree, test_tree)
 
 	# test get file branch
-	def test_getFileBranch_correct(self):
-		pass
+	def test_getFileBranch_correctRoot(self):
+		test_tree = self.server.get_files_branch(self.root + '.testing/')
+		valid_tree = {'folder': {}, '.': ['test.txt'], '..': ['']}
+
+		self.assertIsNotNone(test_tree)
+		self.assertDictEqual(valid_tree, test_tree)
+
+	# test get file branch
+	def test_getFileBranch_correctBranch(self):
+		test_tree = self.server.get_files_branch(self.root + '.testing/folder')
+		valid_tree = {'.': ['file.txt'], '..': ['folder']}
+
+		self.assertIsNotNone(test_tree)
+		self.assertDictEqual(valid_tree, test_tree)
 
 	# test is child path true
 	def test_isChildPath_valid(self):
