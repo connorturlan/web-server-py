572732a6b367702dff6cc6d8d16413fb2a590487
Connor Turlan <connorturlan@gmail.com>
2022-09-26 09:45:04 +0930

    scaffolding create tests

--- tests/FileServer/unit tests/create_test.py
+++ tests/FileServer/unit tests/create_test.py
@@ -14,13 +14,32 @@ class FileServerCreateUnitTests(FileServerCommonTest):
 		self.assertTrue(os.path.exists(self.root + '.testing/create'))
 
 	# test folder create fails - folder exists
+	def test_folder_creation_fails_folderExists(self):
+		pass
+
 	# test folder create fails - directory doesn't exist
+	def test_folder_create_fails_parentDirectoryDoesntExist(self):
+		pass
+
 	# test folder create fails - file not in share directory
+	def test_folder_create_fails_fileNotInShareDirectory(self):
+		pass
 
 	# test file creation succeeds
+	def test_file_create_succeeds(self):
+		pass
+
 	# test file creation fails - file exists
+	def test_file_create_fails_fileExists(self):
+		pass
+
 	# test file creation fails - directory doesn't exist
+	def test_file_create_fails_parentDirectoryDoesntExist(self):
+		pass
+
 	# test file create fails - file not in share directory
+	def test_file_create_fails_fileNotInShareDirectory(self):
+		pass
 
 
 if __name__ == '__main__':
