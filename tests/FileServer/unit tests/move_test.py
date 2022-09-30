d7e73dbf5f082c19b1c66eaa719d98d795e11a95
Connor Turlan <connorturlan@gmail.com>
2022-09-30 10:01:21 +0930

    removed a block of test

--- tests/FileServer/unit tests/move_test.py
+++ tests/FileServer/unit tests/move_test.py
@@ -27,26 +27,6 @@ class FileServerMoveUnitTests(FileServerCommonTest):
 		shutil.rmtree(self.files + 'dst\\')
 
 	# test file move succeeds
-	# test file move succeeds - rename
-	# test file move succeeds - new directory
-	# test file move succeeds - new directory and rename
-	# test file move fails - same path
-	# test file move fails - file doesn't exist
-	# test file move fails - destination doesn't exist
-	# test file move fails - source not in share directory
-	# test file move fails - destination not in share directory
-
-	# test folder move succeeds
-	# test folder move succeeds - rename
-	# test folder move succeeds - new directory
-	# test folder move succeeds - new directory and rename
-	# test folder move fails - same path
-	# test folder move fails - folder doesn't exist
-	# test folder move fails - destination doesn't exist
-	# test folder move fails - source not in share directory
-	# test folder move fails - destination not in share directory
-
-	# test file move succeeds
 	def test_file_move_succeeds(self):
 		success = self.server.move_file(DummyRouter(), 'src/move.txt',
 		                                'dst/move.txt')
